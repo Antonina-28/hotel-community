966cdc09e4466defee59fdac3d14e0140c82c3d8
Antonina Sorokan <atretyakova28@yandex.ru>
2024-12-11 16:35:10 +0300

    Добавляет стили для формы, стили для блока hotels

--- src/js/generate-hotels.js
+++ src/js/generate-hotels.js
@@ -4,17 +4,33 @@ export function generateHotels(hotels) {
   hotels.forEach(hotel => {
     const itemHotel = document.createElement('li');
     const imageHotel = document.createElement('img');
+    const content = document.createElement('div');
+    const description = document.createElement('p');
     const titleHotel = document.createElement('h3');
+    const price = document.createElement('p');
+    const rating = document.createElement('p');
 
+    itemHotel.setAttribute('class', 'hotels__item');
     imageHotel.setAttribute('class', 'hotels__image');
     imageHotel.setAttribute('src', hotel.img);
     imageHotel.setAttribute('width', 280);
     imageHotel.setAttribute('height', 250); 
-    imageHotel.setAttribute('alt', 'Фото отеля.');   
+    imageHotel.setAttribute('alt', 'Фото отеля.');
+    content.setAttribute('class', 'hotels__content');
+    description.textContent = hotel.description;
+    description.setAttribute('class', 'hotels__text');
     titleHotel.textContent = hotel.title;
     titleHotel.setAttribute('class', 'hotels__subtitle');
-    itemHotel.appendChild(imageHotel)
-    itemHotel.appendChild(titleHotel)
-    hotelsList.appendChild(itemHotel)
+    price.textContent = hotel.price;
+    price.setAttribute('class', 'hotels__price');
+    rating.textContent = hotel.rating;
+    rating.setAttribute('class', 'hotels__rating');
+    itemHotel.appendChild(imageHotel);
+    itemHotel.appendChild(content);
+    content.appendChild(description);
+    content.appendChild(titleHotel);
+    content.appendChild(price);
+    content.appendChild(rating);
+    hotelsList.appendChild(itemHotel);
   });
 }
---
+++ src/js/generate-stars.js
@@ -0,0 +1,16 @@
+// const ratingParent = document.querySelector('hotels__rating');
+
+// const totalStars = 5;
+// const activeStars = 4;
+
+// for (let i = 0; i < totalStars; i++) {
+//     const star = document.createElement('div');
+//     star.classList.add('star');
+    
+//     // Добавляем класс active для активных звезд
+//     if (i < activeStars) {
+//         star.classList.add('active');
+//     }
+    
+//     ratingContainer.appendChild(star);
+// }
